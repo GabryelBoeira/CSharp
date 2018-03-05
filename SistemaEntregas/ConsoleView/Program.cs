b005fd6cb77749e3d3e0002a0b20ec154f21f550
gabryel <gabryel392@gmail.com>
2018-03-05 20:34:47 -0300

    tela de opções de cadastro de cliente

--- SistemaEntregas/ConsoleView/Program.cs
+++ SistemaEntregas/ConsoleView/Program.cs
@@ -9,8 +9,48 @@ namespace ConsoleView
 {
     class Program
     {
+        private static int MenuCliente()
+        {
+            Console.WriteLine("Escolha uma opção");
+            Console.WriteLine("-- Clientes --");
+            Console.WriteLine("1 - Cadastro de Cliente");
+            Console.WriteLine("2 - Pesquisar Cliente");
+            Console.WriteLine("3 - Editar Cliente");
+            Console.WriteLine("4 - Limpar Tela");
+            Console.WriteLine("5 - Sair");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
         static void Main(string[] args)
         {
+            int opcaoDigita = MenuCliente();
+
+            switch (opcaoDigita)            {
+
+                case 1:
+                    Console.WriteLine("1");
+                    break;
+                case 2:
+                    Console.WriteLine("2");
+                    break;
+                case 3:
+                    Console.WriteLine("3");
+                    break;
+                case 4:
+                    Console.WriteLine("Limpado");
+                    Console.Clear();
+                    break;
+                case 5:
+                    Console.WriteLine("saindo");
+                    Console.ReadKey();
+                    break;
+                default:
+                    Console.WriteLine("Digito Errado");
+                    break;
+            }
+            Console.WriteLine("- geral -");
+           
+
         }
     }
 }
