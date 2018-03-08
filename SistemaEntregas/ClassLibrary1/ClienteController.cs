71d2c03685e6152e843aab945b43d43d52a527fd
gabryel <gabryel392@gmail.com>
2018-03-07 22:09:27 -0300

    criação da camada controler para cliente

---
+++ SistemaEntregas/ClassLibrary1/ClienteController.cs
@@ -0,0 +1,18 @@
+﻿using modelos;
+using System.Collections.Generic;
+
+namespace ClassLibrary1
+{
+    public class ClienteController
+    {
+        static List<Cliente> MeusCliente = new List<Cliente>();
+
+        public void SalvarClientes(Cliente cliente)
+        {
+
+
+        }
+
+
+    }
+}
