ffb202d5231fafe8aab281b56edca358b355646f
GabryelBoeira <gabryel392@gmail.com>
2018-04-11 20:06:48 -0300

    Ligação com Framework pasta DAO

---
+++ SistemaEntregas/modelos/dao/Contexto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modelos.dao
+{
+    public class  Contexto : DbContext 
+    {
+        public Contexto() : base("stringConn")
+        {
+
+        }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+        public DbSet<Entregador> Entregadores { get; set; }
+        public int MyProperty { get; set; }
+
+
+    }
+}
